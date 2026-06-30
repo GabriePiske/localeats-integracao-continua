9725b5f289eb352b4cc72e60fe40468863ec1148
Gabriel Piske <gpiske2016@gmail.com>
2026-06-30 01:03:11 -0300

    Add continuous integration setup

---
+++ app.py
@@ -0,0 +1,5 @@
+def calculate_total(price, quantity):
+    """
+    Calcula o valor total de um pedido.
+    """
+    return price * quantity
---
+++ tests/test_order.py
@@ -0,0 +1,4 @@
+from app import calculate_total
+
+def test_calculate_total():
+    assert calculate_total(20, 3) == 60
