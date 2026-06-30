67a5139c36dc32afc87e37daeda5256fd37f1f1f
Gabriel Piske <gpiske2016@gmail.com>
2026-06-30 01:22:19 -0300

    Fix test import path

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import calculate_total
+
 
 def test_calculate_total():
     assert calculate_total(20, 3) == 60
